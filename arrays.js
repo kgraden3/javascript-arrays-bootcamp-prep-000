7f37156b18e9fdba926dcc5ecea2c01a6d927a58
Kyle Graden <kylegraden3@gmail.com>
2019-02-12 01:58:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = new array('snickers', 'hundred grand', 'kitkat', 'skittles')
+var starbursts = "starbursts"
+
+function addElementToBeginningOfArray(chocolateBars,starbursts) {
+  return [b, ...chocolateBars]
+}
+
+function destructivelyAddElementToBeginningOfArray(a,b) {
+  return chocolateBars.unshift(b)
+}
+
+function accessElementInArray(chocolateBars,3){
+  return console.log(chocolateBars(3))
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.shift()
+}
+
+function removeElementFromBeginningOfArray(chocolateBars){
+  return chocolateBars.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  return chocolateBars.pop()
+}
+
+function removeElementFromEndOfArray(chocolateBars){
+  return chocolateBars.slice(0, chocolateBars.length - 1)
+}
